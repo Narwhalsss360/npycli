ef6296381861fac2f8b9bf4ffe9aa3d1f863c902
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-08-19 20:02:52 -0400

    Using parameters for better help

--- examples/user_items.py
+++ examples/user_items.py
@@ -89,16 +89,144 @@ def show_cmd(key: Optional[str] = None) -> str:
 
 
 @cli.cmd(name='help', help='Show help for a command or all commands.')
-def help_cmd(command_name: Optional[str] = None) -> str:
-    result: str = ''
+def help_cmd(
+    command_name: Optional[str] = None,
+    parameter_name: Optional[str] = None,
+    /,
+    extended: bool = False
+) -> str:
+    def type_name(t: type) -> str:
+        if t == NoneType:
+            return str(None)
+        return t.__name__
+
+    def annotation_preview(parameter: CommandParameter) -> str:
+        if parameter.annotation_preview:
+            return parameter.annotation_preview
+        out: str = ""
+        for i, t in enumerate(parameter.argument_types):
+            out += type_name(t)
+            if i != len(parameter.argument_types) - 1:
+                out += " | "
+        return out
+
+    def default_preview(parameter: CommandParameter) -> str | None:
+        if parameter.default == parameter.empty:
+            return None
+        if parameter.default_preview:
+            return parameter.default_preview
+        return repr(parameter.default)
+
+    def basic_parameter_help(parameter: CommandParameter) -> str:
+        if (
+            parameter.default != parameter.empty and
+            parameter.argument_types[0] == bool and
+            parameter.kind in (ParameterKind.POSITIONAL_OR_KEYWORD, ParameterKind.KEYWORD_ONLY)
+        ):
+            return (
+                "["
+                    f"{parameter.name}: "
+                    f"{annotation_preview(parameter)} = {default_preview(parameter)}"
+                "]"
+            )
+        else:
+            return (
+                "<"
+                f"{"*" if parameter.kind == ParameterKind.VAR_POSITIONAL else ""}"
+                f"{"**" if parameter.kind == ParameterKind.VAR_KEYWORD else ""}"
+                f"{parameter.name}: "
+                f"{annotation_preview(parameter)}"
+                f"{"" if (default := default_preview(parameter)) is None else f" = {default}"}"
+                ">"
+            )
+
+    def extended_parameter_help(parameter: CommandParameter) -> str:
+        tabstr = " " * TAB_WIDTH
+        out = tabstr
+        for i, name in enumerate(parameter.names):
+            out += name
+            if i != len(parameter.names) - 1:
+                out += " "
+        out += "\n"
+
+        tabstr = " " * TAB_WIDTH * 2
+        out += f"{tabstr}Kind: {parameter.kind.name}\n"
+        out += f"{tabstr}Annotation: {annotation_preview(parameter)}\n"
+
+        out += f"{tabstr}Types:\n"
+        for i, t in enumerate(parameter.argument_types):
+            out += f"{tabstr}{" " * TAB_WIDTH}{type_name(t)}"
+            if i != len(parameter.argument_types) - 1:
+                out += "\n"
+
+        if (default := default_preview(parameter)) is not None:
+            out += f"\n{tabstr}Default: {default}"
+
+        if parameter.parse_hooks is not None:
+            out += f"\n{tabstr}Parse Hooks:"
+            if parameter.parse_hooks.pre is not None:
+                out += f"{tabstr}{" " * TAB_WIDTH}Pre-Hook: {getattr(parameter.parse_hooks.pre, "__name__", "...")}"
+            if parameter.parse_hooks.post is not None:
+                out += f"{tabstr}{" " * TAB_WIDTH}Post-Hook: {getattr(parameter.parse_hooks.post, "__name__", "...")}"
+            if parameter.parse_hooks.err is not None:
+                out += f"{tabstr}{" " * TAB_WIDTH}Error-Hook: {getattr(parameter.parse_hooks.err, "__name__", "...")}"
+
+        if parameter.description:
+            out += f"{tabstr}Description: {parameter.description.replace("\n", f"\n{tabstr}")}"
+
+        return out
+
+    def basic_command_help(command: Command) -> str:
+        out: str = f"{command.name} "
+        last_positional_only_index: int = -1
+        for i, parameter in enumerate(command.parameters):
+            if parameter.kind == ParameterKind.POSITIONAL_ONLY:
+                last_positional_only_index = i
+            else:
+                break
+
+        for i, parameter in enumerate(command.parameters):
+            out += basic_parameter_help(parameter)
+            if i == last_positional_only_index:
+                out += " /"
+            if i != len(command.parameters) - 1:
+                out += " "
+        return out
+
+    def extended_command_help(command: Command) -> str:
+        out: str = ""
+        for i, name in enumerate(command.names):
+            out += name
+            if i != len(command.names) - 1:
+                out += " "
+        out += "\n"
+
+        out += "Parameters:\n"
+        for i, parameter in enumerate(command.parameters):
+            out += extended_parameter_help(parameter)
+            if i != len(command.parameters) - 1:
+                out += "\n"
+
+        return out
+
+    command_help, parameter_help = (extended_command_help, extended_parameter_help) if extended else (basic_command_help, basic_parameter_help)
+
     if command_name is None:
-        for command in cli.commands:
-            result += f'{command}\n'
-    else:
-        if (command := cli.get_command(command_name)) is None:
-            raise CommandDoesNotExistError(f"{command_name} is not a command")
-        return command.details.replace('\t', '\n')
-    return result
+        out: str = ""
+        for i, command in enumerate(cli.commands):
+            out += f"{command_help(command)}"
+            if i != len(cli.commands) - 1:
+                out += "\n"
+        return out
+
+    if (command := cli.get_command(command_name)) is None:
+        return f"{command_name} is not a command."
+
+    if parameter_name is not None:
+        if (parameter := next(filter(lambda p: parameter_name in p.names, command.parameters)), None) is None:
+            return f"'{parameter_name}' is not a parameter"
+        return parameter_help(parameter)
+    return command_help(command)
 
 
 @cli.retvals()
@@ -118,6 +246,9 @@ def as_prompter() -> None:
     while True:
         try:
             cli.prompt()
+        except KeyboardInterrupt:
+            print("\n^C")
+            return
         except EmptyEntriesError:  # If the user entered nothing, just continue
             pass
         except CLIError as err:  # Catch errors that occur between here and execution of command
--- tests/better_help.py
+++ tests/better_help.py
@@ -58,22 +58,81 @@ def delete_item(key: str) -> str:
 
 
 @cli.cmd(name='help')
-def show_help() -> str:
-    names: list[str] = []
-    longest_name_length: int = 0
-    descriptions: list[str] = []
-    for command in cli.commands:
-        if len(command.name) > longest_name_length:
-            longest_name_length = len(command.name)
-        names.append(command.name)
-        descriptions.append(command.details or "")
+def show_help(
+    command_name: str | None = None,
+    parameter_name: Annotated[str | None, Description("Show help of a specific command parameter")] = None,
+    /,
+    extended: bool = False
+) -> str:
+    if command_name is None:
+        names: list[str] = []
+        longest_name_length: int = 0
+        descriptions: list[str] = []
+        for command in cli.commands:
+            if len(command.name) > longest_name_length:
+                longest_name_length = len(command.name)
+            names.append(command.name)
+            descriptions.append(command.details or "")
 
-    name_fmt = f"<{longest_name_length + 2}"
+        name_fmt = f"<{longest_name_length + 2}"
 
-    help_str = ''
-    for name, description in zip(names, descriptions):
-        help_str += f"{format(name, name_fmt)}| {description}\n"
-    return help_str
+        help_str = ''
+        for name, description in zip(names, descriptions):
+            help_str += f"{format(name, name_fmt)}| {description}\n"
+        help_str += "Send KeyboardInterrupt (SIGINT), CTRL+C to exit"
+        return help_str
+
+    if (command := cli.get_command(command_name)) is None:
+        return f"'{command_name}' is not a command."
+
+    out: str = ""
+    for i, name in enumerate(command.names):
+        out += name
+        if i != len(command.names) - 1:
+            out += " "
+    out += "\n"
+
+    def parameter_details(parameter: CommandParameter) -> str:
+        out: str = ""
+        for i, name in enumerate(parameter.names):
+            out += name
+            if i == len(parameter.names) - 1:
+                out += " ->"
+            out += " "
+
+        for i, t in enumerate(parameter.argument_types):
+            out += str(None) if t == NoneType else t.__name__
+            if i != len(parameter.argument_types) - 1:
+                out += " "
+
+        out += f"\nKind: {parameter.kind.name}\n"
+        if parameter.default_preview:
+            out += f"Default: {parameter.default_preview}"
+        elif parameter.default != parameter.empty:
+            out += f"Default: {repr(parameter.default)}"
+
+        if parameter.description:
+            out += f"\n{parameter.description}"
+        return out
+
+    if parameter_name is None:
+        if extended:
+            for i, parameter in enumerate(command.parameters):
+                out += parameter_details(parameter).replace("\n", f"\n{TAB_CHARS}")
+                if i != len(command.parameters) - 1:
+                    out += "\n"
+
+        else:
+            for i, parameter in enumerate(command.parameters):
+                out += f"{TAB_CHARS}{parameter.name}: {"".join(f"{str(None) if t == NoneType else t.__name__} " for t in parameter.argument_types)} {parameter.description}"
+                if i != len(command.parameters) - 1:
+                    out += "\n"
+        return out
+
+    if (parameter := next(filter(lambda p: parameter_name in p.names, command.parameters), None)) is None:
+        return f"'{parameter_name}' is not a parameter of '{command_name}'"
+
+    return f"{out}{parameter_details(parameter)}"
 
 
 @cli.retvals()
@@ -98,7 +157,6 @@ def main() -> None:
             ...
         except KeyboardInterrupt:
             print("\n^C")
-            
             break
 
 
