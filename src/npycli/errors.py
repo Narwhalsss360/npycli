c1d250930e7f61604ddad83b5660414bd6b5ca6e
Franklyn Vasquez <franklynvasquez24@gmail.com>
2026-03-01 20:30:28 -0500

    Added better typing and removed unnecessary super() args

--- src/npycli/errors.py
+++ src/npycli/errors.py
@@ -1,4 +1,4 @@
-from typing import Optional, TYPE_CHECKING
+from typing import Any, Optional, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from .cli import CLI
@@ -6,37 +6,37 @@ if TYPE_CHECKING:
 
 
 class ParsingError(Exception):
-    def __init__(self, *args) -> None:
-        super(ParsingError, self).__init__(*args)
+    def __init__(self, *args: Any) -> None:
+        super().__init__(*args)
 
 
 class MissingKeywordArgumentValueError(ParsingError):
-    def __init__(self, *args) -> None:
-        super(MissingKeywordArgumentValueError, self).__init__(*args)
+    def __init__(self, *args: Any) -> None:
+        super().__init__(*args)
 
 
 class TooManyArgumentsError(ParsingError):
-    def __init__(self, *args) -> None:
-        super(TooManyArgumentsError, self).__init__(*args)
+    def __init__(self, *args: Any) -> None:
+        super().__init__(*args)
 
 
 class CLIError(Exception):
-    def __init__(self, *args, cli=None, command=None) -> None:
-        super(CLIError, self).__init__(*args)
+    def __init__(self, *args: Any, cli: Optional[CLI] = None, command: Optional[Command] = None) -> None:
+        super().__init__(*args)
         self.cli: Optional[CLI] = cli
         self.command: Optional[Command] = command
 
 
 class EmptyEntriesError(CLIError):
-    def __init__(self, *args, **kwargs) -> None:
-        super(EmptyEntriesError, self).__init__(*args, **kwargs)
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
 
 
 class CommandDoesNotExistError(CLIError):
-    def __init__(self, *args, **kwargs) -> None:
-        super(CommandDoesNotExistError, self).__init__(*args, **kwargs)
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
 
 
 class CommandArgumentError(CLIError):
-    def __init__(self, *args, **kwargs) -> None:
-        super(CommandArgumentError, self).__init__(*args, **kwargs)
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
