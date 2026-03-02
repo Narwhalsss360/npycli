2bd27e44262b0f23f0651de2aec3c4029c9a7ead
Franklyn Vasquez <franklynvasquez24@gmail.com>
2026-03-01 20:32:17 -0500

    Added better typing to kwarg_aliasing

--- src/npycli/kwarg_aliasing.py
+++ src/npycli/kwarg_aliasing.py
@@ -6,19 +6,19 @@ from .command import Command, cmd_hook
 ALIASES_ATTR = '__kwarg_aliases__'
 
 
-def attach_kwarg_aliases(func: Callable, aliases: dict[str, tuple[str, ...]]) -> Callable:
+def attach_kwarg_aliases(func: Callable[..., Any], aliases: dict[str, tuple[str, ...]]) -> Callable[..., Any]:
     setattr(func, ALIASES_ATTR, aliases)
     return func
 
 
-def get_kwarg_aliases(func: Callable) -> dict[str, tuple[str, ...]]:
+def get_kwarg_aliases(func: Callable[..., Any]) -> dict[str, tuple[str, ...]]:
     if hasattr(func, ALIASES_ATTR):
         return getattr(func, ALIASES_ATTR)
     return {}
 
 
 def parse_kwarg_aliases(kwargs: dict[str, Any], aliases: dict[str, tuple[str, ...]]) -> dict[str, Any]:
-    parsed_kwargs = {}
+    parsed_kwargs: dict[str, str] = {}
     for kwarg, value in kwargs.items():
         for original_kwarg, original_kwarg_aliases in aliases.items():
             if kwarg in original_kwarg_aliases or original_kwarg == kwarg:
@@ -27,12 +27,12 @@ def parse_kwarg_aliases(kwargs: dict[str, Any], aliases: dict[str, tuple[str, ..
     return parsed_kwargs
 
 
-def alias_kwargs(aliases: dict[str, tuple[str, ...]]) -> Callable[[Callable], Callable]:
-    def decorator(func: Callable) -> Callable:
+def alias_kwargs(aliases: dict[str, tuple[str, ...]]) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
+    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
         attach_kwarg_aliases(func, aliases)
 
         @wraps(func)
-        def wrapper(*args, **kwargs) -> Any:
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             return func(*args, **parse_kwarg_aliases(kwargs, aliases))
 
         return wrapper
@@ -40,8 +40,8 @@ def alias_kwargs(aliases: dict[str, tuple[str, ...]]) -> Callable[[Callable], Ca
     return decorator
 
 
-def alias_cmd_kwargs(aliases: dict[str, tuple[str, ...]]) -> Callable[[Callable], Callable]:
-    def decorator(func: Callable) -> Callable:
+def alias_cmd_kwargs(aliases: dict[str, tuple[str, ...]]) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
+    def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
         def add_detail(command: Command) -> None:
             for kwarg, kwarg_aliases in aliases.items():
                 command.add_detail(f'{kwarg}<->{kwarg_aliases}')
@@ -53,7 +53,7 @@ def alias_cmd_kwargs(aliases: dict[str, tuple[str, ...]]) -> Callable[[Callable]
         attach_kwarg_aliases(func, aliases)
 
         @wraps(func)
-        def wrapper(*args, **kwargs) -> Any:
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             return func(*args, **parse_kwarg_aliases(kwargs, aliases))
 
         return wrapper
