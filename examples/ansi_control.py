054000b84e44bc102eae48ee68f49b158d817f5c
Franklyn Vasquez <franklynvasquez24@gmail.com>
2026-03-01 21:55:17 -0500

    new print_above implementation

--- examples/ansi_control.py
+++ examples/ansi_control.py
@@ -1,5 +1,6 @@
 from typing import Any
 from io import StringIO
+from os import get_terminal_size
 from npycli.ansi import (
     send_ansi,
     SAVE_CURRENT_CURSOR_POSITION,
@@ -14,20 +15,23 @@ from npycli.ansi import (
     SCR_RESET
 )
 
-def print_above(*args: Any, **kwargs: Any) -> None:
+def print_above(*args: Any, max_columns: int, current_is_empty: bool = False, **kwargs: Any) -> None:
     '''
     Print above the current line.
     '''
 
     # Use print with file=buffer so this function can be used just like regular print
     buffer: StringIO = StringIO()
-    current_is_empty = kwargs.pop('current_is_empty') if 'current_is_empty' in kwargs else False
-    print(*args, **kwargs, flush=True, file=buffer)
+    print(*args, **kwargs, flush=True, file=buffer, end="")
     output: str = buffer.getvalue()
 
-    if len(output) > 0 and output[-1] == '\n': # Remove trailing \n, if empty newline wanted, supply 2
-        output = output[:-1]
-    line_count: int = output.count('\n') + 1
+    line_count: int = 1
+    line_length: int = 0
+    for c in output:
+        line_length += 1
+        if line_length == max_columns or c == "\n":
+            line_count += 1
+            line_length = 0
 
     send_ansi(SAVE_CURRENT_CURSOR_POSITION)
     if current_is_empty:
@@ -35,7 +39,7 @@ def print_above(*args: Any, **kwargs: Any) -> None:
         send_ansi(CURSOR_UP.with_args(2))
 
     # These ansi control commands may be supplied with arguments
-    print()
+    print("\n" * line_count, end="")
     send_ansi(CURSOR_UP.with_args(line_count))
     # This one doesn't have argument, so we just repeat the command
     send_ansi(INSERT_NEW_LINE, repeat=line_count)
@@ -50,7 +54,7 @@ def print_above(*args: Any, **kwargs: Any) -> None:
 print(1)
 print(2)
 print(4)
-print_above(3, current_is_empty=True) #This line will be an empty line, so current_is_empty = True
+print_above(3, max_columns=get_terminal_size().columns, current_is_empty=True) #This line will be an empty line, so current_is_empty = True
 
 
 SELECT_CHARACTER_RENDITION(SET_BOLD_MODE, FOREGROND_RED, BACKGROND_WHITE)
@@ -58,5 +62,5 @@ SELECT_CHARACTER_RENDITION(SET_BOLD_MODE, FOREGROND_RED, BACKGROND_WHITE)
 print(1)
 print(2)
 print(4, end='')
-print_above(3)
+print_above(3, max_columns=get_terminal_size().columns)
 SELECT_CHARACTER_RENDITION(SCR_RESET)
--- tests/simple_print_above.py
+++ tests/simple_print_above.py
@@ -1,57 +1,67 @@
 from typing import Any
-from sys import argv
 from io import StringIO
-from threading import Thread
-from time import time, sleep
-from npycli.ansi import send_ansi, SAVE_CURRENT_CURSOR_POSITION, SCROLL_DOWN, CURSOR_UP, INSERT_NEW_LINE, RESTORE_SAVED_CURSOR_POSITION
-from npycli.command import Command
+from os import system, name, get_terminal_size
+from asyncio import run, to_thread, create_task, Task, sleep, CancelledError
+from npycli.ansi import send_ansi, SAVE_CURRENT_CURSOR_POSITION, CURSOR_DOWN, CURSOR_UP, INSERT_NEW_LINE, RESTORE_SAVED_CURSOR_POSITION
 
 
-def print_above(*args: Any, **kwargs: Any) -> None:
+def print_above(*args: Any, max_columns: int, current_is_empty: bool = False, **kwargs: Any) -> None:
+    '''
+    Print above the current line.
+    '''
+
+    # Use print with file=buffer so this function can be used just like regular print
     buffer: StringIO = StringIO()
-    print(*args, **kwargs, flush=True, file=buffer)
+    print(*args, **kwargs, flush=True, file=buffer, end="")
     output: str = buffer.getvalue()
 
-    if len(output) > 0 and output[-1] == '\n':
-        output = output[:-1]
-    line_count: int = output.count('\n') + 1
+    line_count: int = 1
+    line_length: int = 0
+    for c in output:
+        line_length += 1
+        if line_length == max_columns or c == "\n":
+            line_count += 1
+            line_length = 0
 
     send_ansi(SAVE_CURRENT_CURSOR_POSITION)
-    send_ansi(SCROLL_DOWN.with_args(line_count))
+    if current_is_empty:
+        print()
+        send_ansi(CURSOR_UP.with_args(2))
+
+    # These ansi control commands may be supplied with arguments
+    print("\n" * line_count, end="")
     send_ansi(CURSOR_UP.with_args(line_count))
+    # This one doesn't have argument, so we just repeat the command
     send_ansi(INSERT_NEW_LINE, repeat=line_count)
+
+    # Flush, just in case current cursor position gets moved after output
     print(output, end='', flush=True)
     send_ansi(RESTORE_SAVED_CURSOR_POSITION)
+    send_ansi(CURSOR_DOWN.with_args(line_count))
 
 
-def print_on_interval(total: int, interval: int) -> None:
-    sleep(.1)
-    start: float = time()
+PRINT_ABOVE_INTERVAL: float = 1
+
+
+async def main() -> None:
+    system("cls" if name == "nt" else "clear")
+    input_task: Task[str] = create_task(to_thread(input, ">"))
     i: int = 0
-    while time() - start < total:
-        print_above(f'Printed above:\n\titeration:{i}')
+    while True:
+        sleep_task: Task[None] = create_task(sleep(1))
+        input_task.add_done_callback(lambda _: sleep_task.cancel())
+        try:
+            await sleep_task
+        except CancelledError:
+            if not input_task.done():
+                input_task.cancel()
+            break
+        print_above(f"Printed Above\nIteration: {i}", max_columns=get_terminal_size().columns)
         i += 1
-        sleep(interval)
+
+    if input_task.done():
+        print(input_task.result())
 
 
-def main(seconds: float, interval: float) -> None:
-    if seconds <= 0 or interval <= 0:
-        print('seconds and interval must be greater than 0')
-        return
-
-    if interval >= seconds:
-        print('interval must be less than seconds')
-        return
-
-    print(__name__)
-    printer: Thread = Thread(target=print_on_interval, args=(seconds, interval))
-    printer.start()
-    in_str: str = input('>')
-    printer.join()
-    print(in_str)
-
-
-if __name__ == '__main__':
-    if len(argv) == 1:
-        argv.extend(['20', '1'])
-    Command.create(function=main, name='main')(argv[1:])
+if __name__ == "__main__":
+    run(main())
