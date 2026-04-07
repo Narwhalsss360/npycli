9d555a7785f759c28dae701604191ce616580336
Franklyn Vasquez <franklynvasquez24@gmail.com>
2026-04-07 01:29:26 -0400

    Using input_async for simple_print_above

--- tests/simple_print_above.py
+++ tests/simple_print_above.py
@@ -1,9 +1,47 @@
+# NOTE: On windows, this will only work if the input line is on the bottom of the terminal because of windows' line
+# buffering and even then, only single lines. Seems like manual character buffering is the only solution for windows.
+
+from __future__ import annotations  # Required for proper type annotation of multiprocessing.Queue
 from typing import Any, TextIO
 from io import StringIO
 from sys import stdout
 from os import system, name, get_terminal_size
 from asyncio import run, to_thread, create_task, Task, sleep, CancelledError
+from multiprocessing import Process, Queue
+from os import kill
+from signal import SIGINT
+
 from npycli.ansi import SCR_RESET, SELECT_CHARACTER_RENDITION, SET_BOLD_MODE, send_ansi, SAVE_CURRENT_CURSOR_POSITION, CURSOR_DOWN, CURSOR_UP, INSERT_NEW_LINE, RESTORE_SAVED_CURSOR_POSITION, SHOW_CURSOR, strip_ansi, FOREGROUND_RED
+
+
+def input_async_process_target(queue: Queue[str]) -> None:
+    with open(0, "r") as stdin:
+        try:
+            queue.put(stdin.readline().rsplit("\n", 1)[0])
+        except KeyboardInterrupt:
+            return
+
+
+async def input_async(prompt: object = None) -> str:
+    user_input_queue: Queue[str] = Queue(1)
+
+    proc: Process = Process(target=input_async_process_target, args=(user_input_queue,), name="server:input", daemon=True)
+    print(prompt, end="", flush=True)
+    proc.start()
+    assert proc.pid is not None
+
+    try:
+        join_task: Task[None] = create_task(to_thread(proc.join))
+        while user_input_queue.empty() and not join_task.done():
+            await sleep(0.02)
+    except CancelledError:
+        kill(proc.pid, SIGINT)
+        raise
+
+    if user_input_queue.empty():
+        raise EOFError
+
+    return user_input_queue.get()
 
 
 def print_above(*args: Any, max_columns: int, sep: str | None = " ", file: TextIO = stdout, current_is_empty: bool = False, lines_above: int = 1) -> None:
@@ -56,11 +94,11 @@ async def main() -> None:
         f"{SELECT_CHARACTER_RENDITION.with_args(SCR_RESET)}"
         "\n>"
     )
-    input_task: Task[str] = create_task(to_thread(input, input_prompt))
+    input_task: Task[str] = create_task(input_async(input_prompt))
     i: int = 0
     SHOW_CURSOR()
     while True:
-        sleep_task: Task[None] = create_task(sleep(1))
+        sleep_task: Task[None] = create_task(sleep(0.4))
         input_task.add_done_callback(lambda _: sleep_task.cancel())
         try:
             await sleep_task
